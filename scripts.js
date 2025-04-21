44e35a7f3345b1a20d9d27188578aa5b0ec47953
AzDevcoder <adm.azdevcoder@gmail.com>
2025-04-21 15:48:10 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Aqui você pode colocar funcionalidades extras, como scroll suave ou tema dinâmico
+
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Mensagem enviada com sucesso! Em breve entraremos em contato.");
+  });
+  
